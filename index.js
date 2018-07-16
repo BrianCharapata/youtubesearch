c2df4b38d2956814e2c07588c0088d4fdda94687
BrianCharapata <brian@bectek.com>
2018-07-16 16:09:35 -0700

    Updates

--- index.js
+++ index.js
@@ -1,77 +1,50 @@
 'use strict';
 
-// We'll use this URL later to make our request.
-const GH_USERS_ENDPOINT = 'https://api.github.com/users/';
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function handleForm() {
-  const githubForm = $('form[name=github-search]');
-  const usernameField = $('input[name=username]');
-  
-  githubForm.on('submit', function(e) {
-    e.preventDefault();
-    
-    // get the username entered
-    const username = usernameField.val();
-    
-    // pass it in along with the GH endpoint
-    fetchGHData(GH_USERS_ENDPOINT, username);
-    
-    // reset the input
-    usernameField.val('');
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    part: 'snippet',
+    key: 'AIzaSyDEJRKRDL4KuesnMkxDzlgAgx2gfiznIGI',
+    q: `${searchTerm} in:name`,
+    pages: 1,
+    per_page: 2
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+//    .fail(showErr);
+}
+
+function renderResult(result) {
+  return `
+    <div class='searchBox'>
+      <img src=${result.snippet.thumbnails.default.url} alt=${result.snippet.title}>
+      <p>${result.snippet.title}</p>
+    </div>
+  `;
+}
+
+function displayYouTubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  console.log(results);
+  $('.js-search-results').html(results);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
   });
-}
-
-function fetchGHData(baseURL, username) {
-  
-  // make the complete url by concatenating
-  // the endpoint and username together
-  const url = baseURL + username;
-  
-  // try to get some JSON
-  // and show something to the user.
-  $.getJSON(url, showProfileInfo)
-  // ... and show an error if we can't.
-    .fail(showErr);
-}
-
-function showProfileInfo(profileData) {
-  // store the element we'll be appending to
-  const outputElem = $('.js-output');
-  
-  // Store the parts we want from data
-  // using object destructuring
-  let { name, login, html_url } = profileData;
-  
-  // if there's no name in the profile
-  // well inform our user
-  if (!name) name = 'undefined :(';
-  
-  // We'll use the variables above to present
-  // the information we got from GitHub.
-  const profileInfoHTML = (
-    `<div class="user">
-      <p>The user ${login}'s name is ${name}. 
-        <a href="${html_url}" 
-           target="_blank" 
-           rel="noopener noreferrer" 
-           class="user-link">
-          Check out ${login}'s profile!
-        </a></p>
-    </div>`
-  );
-  
-  // then empty the output region
-  // and append our profile info
-  outputElem
-    .prop('hidden', false)
-    .html(profileInfoHTML);
 }
 
 function showErr(err) {
   const outputElem = $('.js-output');
   
   const errMsg = (
-    `<p>We couldn't find a user with that screenname!`
+    `<p>We couldn't find anything on YouTube that matched your entry.`
   );
     
   outputElem
@@ -79,4 +52,4 @@ function showErr(err) {
     .html(errMsg);
 }
 
-$(handleForm);
+$(watchSubmit);
